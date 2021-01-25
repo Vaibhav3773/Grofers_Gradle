94c9accb9b14b4b96393bd58c6612d6636912129
Vaibhav3773 <vaibhav.ghanvat1899@gmail.com>
2021-01-25 12:25:00 +0530

    new

--- src/main/java/com/gf/pageobjects/HomePage.java
+++ src/main/java/com/gf/pageobjects/HomePage.java
@@ -10,5 +10,20 @@ public class HomePage {
     @FindBy(xpath = "//input[@aria-activedescendant="
         + " 'react-select-2--value']")
     private WebElement locationTxtBox;
+    /**
+     * WebElement
+     */
+    @FindBy(xpath="//input[@placeholder='Search for products']")
+    private WebElement searchProdTxtbox;
+    /**
+     * WebElement
+     */
+    @FindBy(xpath="//div[text()='Society Plain Tea (Pouch)']")
+    private WebElement societyTea;
+    /**
+     * WeElement
+     */
+    @FindBy(xpath="(//button[@class='add-to-cart__dec'])")
+    private WebElement addToCartBtn;
 
 }
--- src/test/java/com/gf/regression/suites/SearchProductAddToCart.java
+++ src/test/java/com/gf/regression/suites/SearchProductAddToCart.java
@@ -1,9 +1,53 @@
 package com.gf.regression.suites;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SearchProductAddToCart {
+    ChromeDriver driver;
+ @BeforeTest
+  public void launchBrowser() {
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver();
+  }
+  @AfterTest
+  public void closeBrowser() {
+      driver.close();
+  }
   @Test
-  public void f() {
+  public void searchTeaAddToCart() throws InterruptedException {
+      driver.get("https://grofers.com/");
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      driver.manage().window().maximize();
+      driver.findElement(By.xpath("//button[contains(text(),'Detect')]")).click();
+      Thread.sleep(10000);
+      driver.findElementByXPath("//input[@placeholder='Search for products']")
+      .sendKeys("tea");
+      driver.findElementByXPath("//button[@class='btn search__btn']")
+      .sendKeys(Keys.ENTER);
+      if(driver.findElementByXPath("//div[contains(text(),"
+          + "'Get the best offers via Notifications')]").isDisplayed())
+      {
+          driver.findElementByXPath("//button[@class='No thanks']").click();
+      }
+      if(driver.findElementByXPath("//div/img[@alt="
+          + "'Society Plain Tea (Pouch)']").isDisplayed()) {
+          driver.findElementByXPath("(//div[@class='relative']//button[2])[1]").click();
+      }
+      else
+      {
+          System.out.println("The product is not present");
+      }
+
+      
+      
   }
 }
